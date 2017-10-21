cf7af12572ed8d5d22ec6eea7e755a2854fb7510
Baritus <baritus666@gmail.com>
2017-10-21 17:46:02 -0230

    Initial commit.

---
+++ opentradeengine_TraderCurrencies.sql
@@ -0,0 +1,4 @@
+INSERT INTO opentradeengine.TraderCurrencies (Currency, Balance, Trader, HeldBalance, PendingBalance) VALUES (1, 10000.00000000, 4, 0.00000000, 0.00000000);
+INSERT INTO opentradeengine.TraderCurrencies (Currency, Balance, Trader, HeldBalance, PendingBalance) VALUES (1, 10000.00000000, 5, 0.00000000, 0.00000000);
+INSERT INTO opentradeengine.TraderCurrencies (Currency, Balance, Trader, HeldBalance, PendingBalance) VALUES (2, 10000.00000000, 4, 0.00000000, 0.00000000);
+INSERT INTO opentradeengine.TraderCurrencies (Currency, Balance, Trader, HeldBalance, PendingBalance) VALUES (2, 10000.00000000, 5, 0.00000000, 0.00000000);
